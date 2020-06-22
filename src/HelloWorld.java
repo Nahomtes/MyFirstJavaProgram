90010950daa4676375b9b39acd403a5446727fb8
[Nahom <nahomhaile72@gmail.com>
2020-06-22 17:54:13 -0400

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Nahom.");
+        System.out.println("Today is June 22, 2020. I am alive!");
+    }
+
+}
